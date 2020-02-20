27d55edb5ee070c76c3fa371abb8f84116a311a5
LaisLuderer <38754177+LaisLuderer@users.noreply.github.com>
2020-02-20 15:35:25 -0300

    oi
    two

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processador = Process.Start("powershell.exe");
+            var lab = "03";
+            var pc = "17";
+            var i = 10;
+            var j = 10000;
+            var ganhou = true;
+
+            while (ganhou)
+            {
+                if (i <= 0)
+                {
+                    ganhou = false;
+                }
+                else {
+                    if (j > 0 && i > 0) {
+                        processador.StartInfo.Arguments = ($"msg * /server:lab{lab}-{pc} click {i} vezes em {j} milisegundos\n");
+                        i--;
+                    } else
+                   // if (j < 0 && i <= 0)
+                    {
+                        //processador.StartInfo.Arguments = ($"msg * /server:lab{lab}-{pc} tu e gay man?");
+                        ganhou = false;
+                        processador.StartInfo.Arguments = ($"logoff Console /server:lab{lab}-{pc}\n");
+                    }
+                }
+    
+
+                processador.Start();
+                processador.WaitForExit();
+                processador.Close();
+
+                Thread.Sleep(100);
+                j -= 100;
+            }
+        }
+    }
+}
